d24b40b839e972d9171ba4107dbdf713643f92f1
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-02-21 18:01:30 +0100

    nav almost done

--- front-page.php
+++ front-page.php
@@ -10,8 +10,10 @@
 <body>
     <nav>
         <div class="above">
-            <img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo/LogoCyberDéfense.png" alt="logo portail SODA cyber Défense">
-            <div class="filter"></div>
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo/LogoCyberDéfense.png" alt="logo portail SODA cyber Défense">
+                <!-- <div class="filter"></div> -->
+            </div>
             <div class="connection"> 
                 <form action="<?php echo get_template_directory_uri(); ?>/app/connect.php" method="post">
                     <div> 
--- header.php
+++ header.php
@@ -20,12 +20,65 @@
             assets: '<?php echo get_template_directory_uri(); ?>',
             tests: {}
         });
-        </script>
+		</script>
+		<script src="https://kit.fontawesome.com/06909fb3de.js"></script>
 	</head>
 	<body>
 			<div class="above">
-				<img class="logo" src="<?php echo get_template_directory_uri(); ?>/img/logo/LogoCyberDéfense.png" alt="logo portail SODA cyber Défense">
-				<div class="filter"></div>
+				<div class="logo">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo/LogoCyberDéfense.png" alt="logo portail SODA cyber Défense">
+					<!-- <div class="filter"></div> -->
+				</div>
+				<div class="search">
+					<!-- <i class="fas fa-arrow-left"></i> -->
+					<div class="icons">
+						<i class="fas fa-search"></i>
+						<i class="far fa-bell"></i>
+					</div>
+					<div class="profile_pic">
+						<div class="circle">
+							<img src="" alt="photo de votre profil">
+						</div>
+					</div>
+					<div class="settings"></div>
+				</div>
+			
 			</div>
 			<div class="side">
+				<div class="home">
+					<i class="fas fa-home"></i>
+					<!-- <p>Accueil</p> -->
+                    <i class="fas fa-sort-down"></i>
+				</div>
+				<div class="articles">
+					<i class="far fa-newspaper"></i>
+					<!-- <p>Articles</p> -->
+                    <i class="fas fa-sort-down"></i>
+				</div>
+				<div class="modules">
+					<i class="fas fa-graduation-cap"></i>
+					<!-- <p>Modules</p> -->
+                    <i class="fas fa-sort-down"></i>
+				</div>
+				<div class="tools">
+					<i class="fas fa-tools"></i>
+					<!-- <p>Outils</p> -->
+					<i class="fas fa-sort-down"></i>
+				</div>
+				<div class="quiz">
+					<i class="fas fa-question-circle"></i>
+					<!-- <p>Quiz</p> -->
+					<i class="fas fa-sort-down"></i>
+				</div>
+				<div class="games">
+					<i class="fas fa-gamepad"></i>
+					<!-- <p>Jeux</p> -->
+					<i class="fas fa-sort-down"></i>
+				</div>
+				<div class="rank">
+					<i class="fas fa-trophy"></i>
+					<!-- <p>Classement</p> -->
+					<i class="fas fa-sort-down"></i>
+				</div>
+				<i class="fas fa-question"></i>
 			</div>
