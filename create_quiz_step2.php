7fdf34f4433432a2e3544245196e66b698a67c74
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-02-28 17:54:52 +0100

    create quiz step 2 ->10 questions & adding a question in progress

--- create_quiz_step2.php
+++ create_quiz_step2.php
@@ -24,44 +24,93 @@
         }
     ?> -->
     <form action="">
-        <!-- <?php
+        <?php
         
-            for($i=1; $i<10; $i++){
-                echo '<div class="question">
-                    <label></label>
-                    <input type="text" name="">
-                    <label></label>
-                </div>';
+            for($i=1; $i<=10; $i++){
+                echo '
+                <div class="question">
+                    <input type="number" name="nbrQuestion" hidden>
+                    <div>
+                        <label for="">Votre question: '.$i.'.</label>
+                        <input type="text" name="question">
+                    </div>
+                    <div class="answers">
+                        <label for="">Vos réponses(2 minimum):</label>
+                        <div class="abcd">
+                            <div class="answer">
+                                <label for="">A.</label>
+                                <input type="text" name="answerA">
+                                <label class="true" id="truea">
+                                    <input type="radio" value="true" name="ansA'.$i.'">
+                                    <span>
+                                        <i class="fas fa-check"></i>
+                                    </span>
+                                </label>
+                                <label class="false" id="falsea">
+                                    <input type="radio" value="false" name="ansA'.$i.'">
+                                    <span>
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </label>
+                            </div>
+                            <div class="answer">
+                                <label for="">B.</label>
+                                <input type="text" name="answerB">
+                                <label class="true" id="trueb">
+                                    <input type="radio" value="true" name="ansB'.$i.'">
+                                    <span>
+                                        <i class="fas fa-check"></i>
+                                    </span>
+                                </label>
+                                <label class="false" id="falseb">
+                                    <input type="radio" value="false" name="ansB'.$i.'">
+                                    <span>
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </label>
+                            </div>
+                            <div class="answer">
+                                <label for="">C.</label>
+                                <input type="text" name="answerC">
+                                <label class="true" id="truec">
+                                    <input type="radio" value="true" name="ansC'.$i.'">
+                                    <span>
+                                        <i class="fas fa-check"></i>
+                                    </span>
+                                </label>
+                                <label class="false" id="falsec">
+                                    <input type="radio" value="false" name="ansC'.$i.'">
+                                    <span>
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </label>
+                            </div>
+                            <div class="answer">
+                                <label for="">D.</label>
+                                <input type="text" name="answerD">
+                                <label class="true" id="trued">
+                                    <input type="radio" value="true" name="ansD'.$i.'">
+                                    <span>
+                                        <i class="fas fa-check"></i>
+                                    </span>
+                                </label>
+                                <label class="false" id="falsed">
+                                    <input type="radio" value="false" name="ansD'.$i.'">
+                                    <span>
+                                        <i class="fas fa-times"></i>
+                                    </span>
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                ';
             }
 
-        ?> -->
-        <div class="question">
-            <div>
-                <label for="">Votre question: 1.</label>
-                <input type="text" name="question">
-            </div>
-            <div>
-                <label for="">Vos réponses(2 minimum):</label>
-                <div id="a">
-                    <label for="">A.</label>
-                    <input type="text" name="">
-                </div>
-                <div id="b">
-                    <label for="">B.</label>
-                </div>
-                <div id="c">
-                    <label for="">C.</label>
-                </div>
-                <div id="d">
-                    <label for="">D.</label>
-                </div>
-            </div>
-        </div>
-        
+        ?> 
     </form>
 
     <i class="fas fa-plus"></i>
 
 </div>
-
-<?php get_footer(); ?>
+<?php get_footer()?>
