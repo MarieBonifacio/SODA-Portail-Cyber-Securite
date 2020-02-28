2d0209584360b0b9e999a55eafa369f48db345a1
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-02-28 13:16:23 +0100

    fix create quiz redirection

--- app/create_quiz_1.php
+++ app/create_quiz_1.php
@@ -64,5 +64,5 @@ if(!empty($_POST['title']) && !empty($_POST['theme']))
 $_SESSION["errorQuiz"] = $error_quiz;
 $_SESSION["quizOk"] = $quiz_ok;
 print_r($quiz);
-// wp_redirect( home_url().'/create-quiz-1' );
+wp_redirect( home_url().'/create-quiz-1' );
 ?>
