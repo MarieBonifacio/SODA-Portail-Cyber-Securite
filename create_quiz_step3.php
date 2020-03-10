3271099898fbb469a5394fc3a766c5c29e201b38
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-03-10 12:51:10 +0100

    quiz step 3 started-> front

--- create_quiz_step3.php
+++ create_quiz_step3.php
@@ -1,3 +1,30 @@
 <?php /* Template Name: Create Quiz Etape 3 */ get_header();?>
 
+<h2>Titre du quiz</h2>
+
+<div class="step3">
+
+  <h3>Étape 3: Confirmation</h3>
+
+  <div class="steps">
+      <div class="step">1</div>
+      <div class="step">2</div>
+      <div class="step stepInto">3</div>
+      <div class="stick"></div>
+  </div>
+
+  <div class="recap">
+    <div class="question">
+      <div>
+        <span>1</span>
+        <p>Voici la question 1, quel est la réponse ?</p>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  </div>
+
+</div>
+
 <?php get_footer();?>
