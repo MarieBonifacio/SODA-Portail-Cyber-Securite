8b42ebe9193a83829e4d3eb139c9b9a65ce0c21d
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-03-10 10:14:38 +0100

    correction of bug with git

--- app/create_quiz_2.php
+++ app/create_quiz_2.php
@@ -27,9 +27,9 @@ if($nbrQuestion >= 10)
             //img/vidéo vides de base
 
             $question['info'] = array(
-                 'text' => $_POST['question_'.$i],
-                 'img' => '',
-                 'video' => '',
+                'text' => $_POST['question_'.$i],
+                'img' => $_FILES['q_'.$i.'_img'],
+                'video' => $_POST['q_'.$i.'video'],
             );
             
             //si le lien d'une vidéo est donné
@@ -38,20 +38,27 @@ if($nbrQuestion >= 10)
             }
             
             //Si une image est donnée
-            if(!empty($_FILES['q_'.$i.'_img'])){
+            if(!isset($_FILES['q_'.$i.'_img']) || $_FILES['q_'.$i.'_img']['error'] == UPLOAD_ERR_NO_FILE)
+            {
+                $error_quiz = "Veuillez selectionner une image en format jpg ou png.";
+                wp_redirect( home_url().'/creationquizetape2' );
+            }else{
+
                 // FAUT FAIRE LE TRAITEMENT UPLOAD => DEPLACEMENT + RENOMMAGE
                 $content_dir =  get_template_directory().'/img/quizs/questions';
                 $tmp_file = $_FILES['q_'.$i.'_img']['tmp_name'];
         
                 if(!is_uploaded_file($tmp_file))
                 {
-                    $error_quiz="Le fichier est introuvable";
+                    $error_quiz="Un fichier est introuvable";
+                    wp_redirect( home_url().'/creationquizetape2' );
                 }
                 $type_file = $_FILES['q_'.$i.'_img']['type'];
         
                 if( !strpos($type_file, 'jpg') && !strpos($type_file, 'jpeg') && !strpos($type_file, 'png')) 
                 {
-                    $error_quiz = "Le format du fichier n'est pas pris en charge";
+                    $error_quiz = "Le format d'un des fichiers n'est pas pris en charge";
+                    wp_redirect( home_url().'/creationquizetape1' );
                 }
                     // on copie le fichier dans le dossier de destination
                 $name_file = $_POST['q_'.$i.'_img'].'.'.preg_replace("#image\/#","",$type_file);
@@ -60,6 +67,7 @@ if($nbrQuestion >= 10)
                 if( !move_uploaded_file($tmp_file, $content_dir . $name_file) )
                 { 
                     $errorQuiz = "Impossible de copier le fichier $name_file dans $content_dir";
+                    wp_redirect( home_url().'/creationquizetape2' );
                 }
         
                 // Puis stocker le resultat $question[$i]['img'] => $_POST['q_'.$i.'_img'];
@@ -80,7 +88,7 @@ if($nbrQuestion >= 10)
                 //Si le texte de la réponse est rempli
                 if(!empty($_POST['q_'.$i.'_reponse_'.$r])){
                      //si la réponse est marque bonne, on incremente le compteur de bonne reponse
-                     if($_POST['q_'.$i.'_isTrue_'.$r] == true){
+                    if($_POST['q_'.$i.'_isTrue_'.$r] == true){
                         $nbrTrue ++;
                     }
                     //on stocke le text et le isTrue dans une variable answer
@@ -90,7 +98,7 @@ if($nbrQuestion >= 10)
                     );
 
                      //on stock le tableau answer dans la liste des reponses ['answers'] de la question ([$i])
-                     $_SESSION['quizData']['questions'][$i]['answers'][$r] = $answer;
+                    $_SESSION['quizData']['questions'][$i]['answers'][$r] = $answer;
                 }
                 /*else{
                     //On renvoie sur la page précedente si une des 4 réponse n'est pas rempli
@@ -136,10 +144,18 @@ print_r($_SESSION['quizData']);
 //recuperation quiz
 $newQuiz = new Quiz();
 $newQuiz->setName($_SESSION['quizData']['quiz']['title']);
+
+//A FAIRE (faire table tags)
 $newQuiz->setTagId(1);
-$author = new User();
-$author->selectById(1);
-$newQuiz->setAuthor( $author->getId() );
+
+//Récup user connecté
+if(!empty($_SESSION['userConnected']))
+{
+    $id = $_SESSION['userConnected'];
+    $author = new User();
+    $author->selectById($id);
+}
+$newQuiz->setAuthor($author);
 $newQuiz->setImgPath($_SESSION['quizData']['quiz']['img']);
 $newQuiz->save();
 $newQuizId = $wpdb->insert_id;
