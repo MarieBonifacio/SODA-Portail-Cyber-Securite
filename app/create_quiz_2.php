c58e9032f8913bd619289bbffa1426e76f201a68
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-03-12 17:00:49 +0100

    json jeu quiz

--- app/create_quiz_2.php
+++ app/create_quiz_2.php
@@ -1,4 +1,5 @@
 <?php
+
 define('WP_USE_THEMES', false);
 require('class/answer.class.php');
 require('class/question.class.php');
@@ -13,171 +14,252 @@ include($path.'wp-load.php');
 
 $nbrQuestion = $_POST['nbrQuestion'];
 $_SESSION['errorQuiz'] = "";
-// print_r($_POST);
 
-// echo '<pre>';
-// print_r($_FILES);
-// echo '</pre>';
-
-
-
-//J'initialise la variable session erreur
 if(!empty($_SESSION['errorQuiz'])){
     unset($_SESSION['errorQuiz']);
 }
+if(!empty($_SESSION['quizData'])){
+    unset($_SESSION['questions'] = null);
+}
 
-//si le nombre de question est supérieur ou égal à 10
-if($nbrQuestion >= 10)
-{
-    //pour chaque question du formulaire
-    for( $i = 1; $i <= $nbrQuestion +50; $i++)
-    {
-        //Si l'énoncé de la question est rempli
-        if(!empty($_POST['question_'.$i]))
-        {
-           
-            //Je rempli en session les champs question, image et vidéo sont vides de base
+if($nbrQuestion >= 10){
+    for( $i = 1; $i <= $nbrQuestion +50; $i++)    {
+        if(!empty($_POST['question_'.$i])){
             $question['info'] = array(
                 'text' => $_POST['question_'.$i],
                 'img' => "",
                 'video' => "",
             );
-            
-            //si le lien d'une vidéo est donné
+
             if(!empty($_POST['q_'.$i.'_video'])){
                 $question['info']['video'] = $_POST["q_'.$i.'_video"];
             }
 
-            //////////////////////////
-            // if(!empty($_FILES['q_'.$i.'_img'])){
-            //     echo "- LOG : image for question ".$i."<br/>";
-            //     $content_dir =  get_template_directory().'/img/quizs/questions/';
-            //     $tmp_file = $_FILES['q_'.$i.'_img']['tmp_name'];
-            //     if(!is_uploaded_file($tmp_file))
-            //     {
-            //         echo "----- LOG :  Le fichier est introuvable<br/>";
-            //     }
-            //     $type_file = $_FILES['q_'.$i.'_img']['type'];
-            //     echo "----- LOG :  type_file : ".$type_file."<br/>";
-            //     if( !strpos($type_file, 'jpg') && !strpos($type_file, 'jpeg') && !strpos($type_file, 'png'))
-            //     {
-            //         echo "----- LOG :  format invalide<br/>";
-            //     }
-            //     $name_file = $_POST["q_'.$i.'_img"].'.'.preg_replace("#image\/#","",$type_file);
-            //     echo "----- LOG :  name_file : ".$name_file."<br/>";
-            //     $img = $name_file;
-            //     if( !move_uploaded_file($tmp_file, $content_dir . $name_file) )
-            //     {
-            //         echo "----- LOG :  error move : ".$name_file." ==> ".$content_dir."<br/>";
-            //         $errorQuiz = "Impossible de copier le fichier $name_file dans $content_dir";
-            //     }
-            // }else{
-            //     echo "- LOG : no image for question ".$i."<br/>";
-            // }
-
-            /////////////////////////////
-            
- 
-            // Si une image est donnée
             if($_FILES['q_'.$i.'_img']['error'] != UPLOAD_ERR_NO_FILE && !empty($_FILES['q_'.$i.'_img']))
             {
                 $dir = $_SESSION['quizData']['quiz']['title'];
-                echo '---'.$dir.'----';
                 mkdir("../img/quizs/".$dir."/questions", 0775, true);
                 $content_dir =  get_template_directory()."/img/quizs/".$dir."/questions/";
                 $tmp_file = $_FILES['q_'.$i.'_img']['tmp_name'];
 
-                if(!is_uploaded_file($tmp_file))
-                {
+                if(!is_uploaded_file($tmp_file)){
                     $error_quiz="Un des fichiers est introuvable";
                     wp_redirect( home_url().'/creationquizetape2' );
                 }
 
                 $type_file = $_FILES['q_'.$i.'_img']['type'];
 
-                if( !strpos($type_file, 'jpg') && !strpos($type_file, 'jpeg') && !strpos($type_file, 'png')) 
-                {
+                if( !strpos($type_file, 'jpg') && !strpos($type_file, 'jpeg') && !strpos($type_file, 'png')){
                     $error_quiz = "Le format d'un des fichiers n'est pas pris en charge.";
                     wp_redirect( home_url().'/creationquizetape2' );
                 }
-                
-                
+
                 $name_file = md5($tmp_file).'.'.preg_replace("#image\/#","",$type_file);
 
-                echo $name_file;
-
-                if( !move_uploaded_file($tmp_file, $content_dir . $name_file) )
-                { 
+                if( !move_uploaded_file($tmp_file, $content_dir . $name_file) ){
                     $errorQuiz = "Impossible de copier le fichier $name_file dans $content_dir";
                     wp_redirect( home_url().'/creationquizetape2' );
                 }
 
                 $img = $name_file;
                 $question['info']['img'] = $img;
-
             }
 
-            //on stocke le tableau questions dans la liste des questions (['questions']) de la variable de session quizdata
             $_SESSION['quizData']['questions'][$i] = $question;
 
-        
             $answers= array();
 
             $nbrAnswer = 0;
             $nbrTrue = 0;
-            //pour chaque réponse à cette question
             for( $r=1; $r <= 4; $r++){
-                //compteur de bonne reponse par question
-                //Si le texte de la réponse est rempli
                 if(!empty($_POST['q_'.$i.'_reponse_'.$r])){
-                    
-                     //si la réponse est marque bonne, on incremente le compteur de bonne reponse
+                    $nbrAnswer += 1;
                     if($_POST['q_'.$i.'_isTrue_'.$r] == true){
-                        $nbrTrue ++;
+                        $nbrTrue += 1;
                     }
-                    //on stocke le text et le isTrue dans une variable answer
                     $answer = array(
                         'text' => $_POST['q_'.$i.'_reponse_'.$r],
                         'isTrue' => $_POST['q_'.$i.'_isTrue_'.$r],
                     );
-
-                     //on stock le tableau answer dans la liste des reponses ['answers'] de la question ([$i])
                     $_SESSION['quizData']['questions'][$i]['answers'][$r] = $answer;
                 }
-                /*else{
-                    //On renvoie sur la page précedente si une des 4 réponse n'est pas rempli
-                    $_SESSION['errorQuiz'] = "Veuillez remplir 4 réponses par questions.";
-                    //wp_redirect( home_url().'/create_quiz_2' );
-                }*/
-                
-                //si plus d'une reponse à été mise comme bonne pour la question
-                // if($nbrTrue != 1)
-                // {
-                //     $_SESSION['errorQuiz'] = "Il faut une unique bonne réponse par question.";
-                //     //wp_redirect( home_url().'/creationquizetape2' );
-                // }
+
+                if($nbrTrue != 1)
+                {
+                    $_SESSION['errorQuiz'] = "Il faut une unique bonne réponse par question.";
+                }
                 if($nbrAnswer < 2)
                 {
                     $_SESSION['errorQuiz'] = "Il faut deux réponses minimum par question.";
-                    wp_redirect( home_url().'/creationquizetape2' );
                 }
             }
         }else{
-            //on renvoie sur la page precedente si une question n'a pas d'énoncé
             $_SESSION['errorQuiz'] = "Veuillez remplir l'énoncé des questions.";
-            wp_redirect( home_url().'/creationquizetape2' );
         }
     }
-    
-} else {
+}else{
     $_SESSION['errorQuiz'] = "Veuillez créer au moins 10 questions.";
-    wp_redirect( home_url().'/creationquizetape2' );
 }
-//quand toutes les questions/réponses on été traitées on renvoie à la page suivante
 
 if($_SESSION['errorQuiz'] == ""){
     wp_redirect( home_url().'/creationquizetape3' );
 }
+wp_redirect( home_url().'/creationquizetape2' );
+
+
+
+?>
+
+
+
+// define('WP_USE_THEMES', false);
+// require('class/answer.class.php');
+// require('class/question.class.php');
+// require('class/quiz.class.php');
+// require('class/quiz_score.class.php');
+// require('class/tag.class.php');
+// require('class/user.class.php');
+
+// $path = preg_replace('/wp-content(?!.*wp-content).*/','',__DIR__);
+// include($path.'wp-load.php');
+
+
+// $nbrQuestion = $_POST['nbrQuestion'];
+// $_SESSION['errorQuiz'] = "";
+// // print_r($_POST);
+
+// // echo '<pre>';
+// // print_r($_FILES);
+// // echo '</pre>';
+
+
+
+// //J'initialise la variable session erreur
+// if(!empty($_SESSION['errorQuiz'])){
+//     unset($_SESSION['errorQuiz']);
+// }
+
+// //si le nombre de question est supérieur ou égal à 10
+// if($nbrQuestion >= 1)
+// {
+//     //pour chaque question du formulaire
+//     for( $i = 1; $i <= $nbrQuestion +50; $i++)
+//     {
+//         //Si l'énoncé de la question est rempli
+//         if(!empty($_POST['question_'.$i]))
+//         {
+           
+//             //Je rempli en session les champs question, image et vidéo sont vides de base
+//             $question['info'] = array(
+//                 'text' => $_POST['question_'.$i],
+//                 'img' => "",
+//                 'video' => "",
+//             );
+            
+//             //si le lien d'une vidéo est donné
+//             if(!empty($_POST['q_'.$i.'_video'])){
+//                 $question['info']['video'] = $_POST["q_'.$i.'_video"];
+//             }
+            
+//             // Si une image est donnée
+//             if($_FILES['q_'.$i.'_img']['error'] != UPLOAD_ERR_NO_FILE && !empty($_FILES['q_'.$i.'_img']))
+//             {
+//                 $dir = $_SESSION['quizData']['quiz']['title'];
+//                 echo '---'.$dir.'----';
+//                 mkdir("../img/quizs/".$dir."/questions", 0775, true);
+//                 $content_dir =  get_template_directory()."/img/quizs/".$dir."/questions/";
+//                 $tmp_file = $_FILES['q_'.$i.'_img']['tmp_name'];
+
+//                 if(!is_uploaded_file($tmp_file))
+//                 {
+//                     $error_quiz="Un des fichiers est introuvable";
+//                     wp_redirect( home_url().'/creationquizetape2' );
+//                 }
+
+//                 $type_file = $_FILES['q_'.$i.'_img']['type'];
+
+//                 if( !strpos($type_file, 'jpg') && !strpos($type_file, 'jpeg') && !strpos($type_file, 'png')) 
+//                 {
+//                     $error_quiz = "Le format d'un des fichiers n'est pas pris en charge.";
+//                     wp_redirect( home_url().'/creationquizetape2' );
+//                 }
+                
+                
+//                 $name_file = md5($tmp_file).'.'.preg_replace("#image\/#","",$type_file);
+
+//                 echo $name_file;
+
+//                 if( !move_uploaded_file($tmp_file, $content_dir . $name_file) )
+//                 { 
+//                     $errorQuiz = "Impossible de copier le fichier $name_file dans $content_dir";
+//                     wp_redirect( home_url().'/creationquizetape2' );
+//                 }
+
+//                 $img = $name_file;
+//                 $question['info']['img'] = $img;
+
+//             }
+
+//             //on stocke le tableau questions dans la liste des questions (['questions']) de la variable de session quizdata
+//             $_SESSION['quizData']['questions'][$i] = $question;
+
+        
+//             $answers= array();
+
+//             $nbrAnswer = 0;
+//             $nbrTrue = 0;
+//             //pour chaque réponse à cette question
+//             for( $r=1; $r <= 4; $r++){
+//                 //compteur de bonne reponse par question
+//                 //Si le texte de la réponse est rempli
+//                 if(!empty($_POST['q_'.$i.'_reponse_'.$r])){
+                    
+//                      //si la réponse est marque bonne, on incremente le compteur de bonne reponse
+//                     if($_POST['q_'.$i.'_isTrue_'.$r] == true){
+//                         $nbrTrue ++;
+//                     }
+//                     //on stocke le text et le isTrue dans une variable answer
+//                     $answer = array(
+//                         'text' => $_POST['q_'.$i.'_reponse_'.$r],
+//                         'isTrue' => $_POST['q_'.$i.'_isTrue_'.$r],
+//                     );
+
+//                      //on stock le tableau answer dans la liste des reponses ['answers'] de la question ([$i])
+//                     $_SESSION['quizData']['questions'][$i]['answers'][$r] = $answer;
+//                 }
+//                 /*else{
+//                     //On renvoie sur la page précedente si une des 4 réponse n'est pas rempli
+//                     $_SESSION['errorQuiz'] = "Veuillez remplir 4 réponses par questions.";
+//                     //wp_redirect( home_url().'/create_quiz_2' );
+//                 }*/
+                
+//                 //si plus d'une reponse à été mise comme bonne pour la question
+//                 // if($nbrTrue != 1)
+//                 // {
+//                 //     $_SESSION['errorQuiz'] = "Il faut une unique bonne réponse par question.";
+//                 //     //wp_redirect( home_url().'/creationquizetape2' );
+//                 // }
+//                 if($nbrAnswer < 2)
+//                 {
+//                     $_SESSION['errorQuiz'] = "Il faut deux réponses minimum par question.";
+//                     wp_redirect( home_url().'/creationquizetape2' );
+//                 }
+//             }
+//         }else{
+//             //on renvoie sur la page precedente si une question n'a pas d'énoncé
+//             $_SESSION['errorQuiz'] = "Veuillez remplir l'énoncé des questions.";
+//             wp_redirect( home_url().'/creationquizetape2' );
+//     }
+    
+// } else {
+//     $_SESSION['errorQuiz'] = "Veuillez créer au moins 10 questions.";
+//     wp_redirect( home_url().'/creationquizetape2' );
+// }
+// //quand toutes les questions/réponses on été traitées on renvoie à la page suivante
+
+// if($_SESSION['errorQuiz'] == ""){
+//     wp_redirect( home_url().'/creationquizetape3' );
+// }
 
 
 ?>
--- test.php
+++ test.php
@@ -1,109 +1,103 @@
 <?php
-header('content-type:application/json'); 
-require('app/class/answer.class.php');
-require('app/class/question.class.php');
-require('app/class/quiz.class.php');
-require('app/class/quiz_score.class.php');
-require('app/class/tag.class.php');
-require('app/class/user.class.php');
 
-$path = preg_replace('/wp-content(?!.*wp-content).*/','',__DIR__);
-include($path.'wp-load.php');
 
-//JSON encode
-$quiz = new Quiz();
-$quiz->selectById(135);
 
-// if(isset($_GET['id'])){
-//     $quiz = new Quiz();
-//     $quiz->selectById($_GET['id']);
+
+
+// //JSON encode
+// $quiz = new Quiz();
+// $quiz->selectById(135);
+
+// // if(isset($_GET['id'])){
+// //     $quiz = new Quiz();
+// //     $quiz->selectById($_GET['id']);
+// // }
+
+// //on récupére les info du quiz et on les met dans un tableau $quiz
+// $quiz = array(
+//     'id' => $quiz->getId(),
+//     'name' => $quiz->getName(),
+//     'tag_id' => $quiz->getTag()->getId(),
+//     'tag_name' => $quiz->getTag()->getName(),
+//     'img' => $quiz->getImgPath(),
+// );
+// // $quiz => [ id , name, tag_id, tag_name, img ] 
+
+// //on récupàre les questions du quiz et on boucle dessus
+// $questions = $wpdb->get_results( "SELECT * FROM question WHERE id_quiz = '$quiz->getId()'");
+// print_r($question);
+// foreach($questions as $q){
+//     //on récupére les infos de la question et on les met dans un tableau $question
+//     $qTmp = new Question();
+//     $qTmp->selectById($q->id);
+//     $question = array(
+//         "id" => $qTmp->getId(),
+//         "id_quiz" => $qTmp->getIdQuiz(),
+//         "content" => $qTmp->getContent(),
+//         "img_path" => $qTmp->getImgPath(),
+//         "url" => $qTmp->getUrl(),
+//         "points" => $qTmp->getPoints(),
+//     );
+//     print_r($question);
+//     // $question => [ id , id_quiz, content, img_path, url, points ] 
+//     //on récupére les réponses de la question et on boucle dessus
+//     $answers = $wpdb->get_results( "SELECT * FROM answer where id_question= '$q->id'" );
+//     foreach($answers as $a){
+//         //on récupére les info de la reponse et on les met dans un tableau $answer
+//         $aTmp = new Answer();
+//         $aTmp->selectById($a->id);
+//         $answer = array(
+//             'id' => $aTmp->getId(),
+//             'id_question' => $aTmp->getIdQuestion(),
+//             'content' => $aTmp->getContent(),
+//             'is_true' => $aTmp->getIsTrue(),
+//         );
+//         // $answer => [ id , id_question, content, is_true ]
+//         print_r($answer);
+//         //  on stock chaque réponse dans un sous tableau de $question
+//         $question['answers'][] = $answer;
+//         print_r( $question);
+//         // $question=> [ 
+//         //     id , 
+//         //     id_quiz, 
+//         //     content, 
+//         //     img_path, 
+//         //     url, 
+//         //     points , 
+//         //     answers[
+//         //         [ id , id_question, content, is_true ],
+//         //         ... (fois le nombre de réponse)
+//         //     ]
+//         // ] 
+//     }
+//     //  on stock chaque question dans un sous tableau de $quiz
+//     $quiz['questions'][] = $question;
+//     // $quiz => [ 
+//     //     id , 
+//     //     name, 
+//     //     tag_id, 
+//     //     tag_name, 
+//     //     img,
+//     //     questions[
+//     //          [ 
+//     //               id , 
+//     //               id_quiz, 
+//     //               content, 
+//     //               img_path, 
+//     //               url, 
+//     //               points , 
+//     //               answers[
+//     //                      [ id , id_question, content, is_true ],
+//     //                      [ id , id_question, content, is_true ],
+//     //                      [ id , id_question, content, is_true ],
+//     //                      [ id , id_question, content, is_true ],
+//     //              ]
+//     //          ],
+//     //          ... (fois le nombre de question)
+//     //     ]
+//     // ] 
 // }
 
-//on récupére les info du quiz et on les met dans un tableau $quiz
-$quiz = array(
-    'id' => $quiz->getId(),
-    'name' => $quiz->getName(),
-    'tag_id' => $quiz->getTag()->getId(),
-    'tag_name' => $quiz->getTag()->getName(),
-    'img' => $quiz->getImgPath(),
-);
-// $quiz => [ id , name, tag_id, tag_name, img ] 
-
-//on récupàre les questions du quiz et on boucle dessus
-$questions = $wpdb->get_results( "SELECT * FROM question WHERE id_quiz = '$quiz->getId()'");
-print_r($question);
-foreach($questions as $q){
-    //on récupére les infos de la question et on les met dans un tableau $question
-    $qTmp = new Question();
-    $qTmp->selectById($q->id);
-    $question = array(
-        "id" => $qTmp->getId(),
-        "id_quiz" => $qTmp->getIdQuiz(),
-        "content" => $qTmp->getContent(),
-        "img_path" => $qTmp->getImgPath(),
-        "url" => $qTmp->getUrl(),
-        "points" => $qTmp->getPoints(),
-    );
-    print_r($question);
-    // $question => [ id , id_quiz, content, img_path, url, points ] 
-    //on récupére les réponses de la question et on boucle dessus
-    $answers = $wpdb->get_results( "SELECT * FROM answer where id_question= '$q->id'" );
-    foreach($answers as $a){
-        //on récupére les info de la reponse et on les met dans un tableau $answer
-        $aTmp = new Answer();
-        $aTmp->selectById($a->id);
-        $answer = array(
-            'id' => $aTmp->getId(),
-            'id_question' => $aTmp->getIdQuestion(),
-            'content' => $aTmp->getContent(),
-            'is_true' => $aTmp->getIsTrue(),
-        );
-        // $answer => [ id , id_question, content, is_true ]
-        print_r($answer);
-        //  on stock chaque réponse dans un sous tableau de $question
-        $question['answers'][] = $answer;
-        print_r( $question);
-        // $question=> [ 
-        //     id , 
-        //     id_quiz, 
-        //     content, 
-        //     img_path, 
-        //     url, 
-        //     points , 
-        //     answers[
-        //         [ id , id_question, content, is_true ],
-        //         ... (fois le nombre de réponse)
-        //     ]
-        // ] 
-    }
-    //  on stock chaque question dans un sous tableau de $quiz
-    $quiz['questions'][] = $question;
-    // $quiz => [ 
-    //     id , 
-    //     name, 
-    //     tag_id, 
-    //     tag_name, 
-    //     img,
-    //     questions[
-    //          [ 
-    //               id , 
-    //               id_quiz, 
-    //               content, 
-    //               img_path, 
-    //               url, 
-    //               points , 
-    //               answers[
-    //                      [ id , id_question, content, is_true ],
-    //                      [ id , id_question, content, is_true ],
-    //                      [ id , id_question, content, is_true ],
-    //                      [ id , id_question, content, is_true ],
-    //              ]
-    //          ],
-    //          ... (fois le nombre de question)
-    //     ]
-    // ] 
-}
-
-echo json_encode($quiz);
+// echo json_encode($quiz);
 
 ?>
