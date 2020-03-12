52e701b6355de911a56cac31a5eeb2bb46230f16
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-03-12 10:24:58 +0100

    ajout tag require

--- app/create_quiz_2.php
+++ app/create_quiz_2.php
@@ -4,6 +4,7 @@ require('class/answer.class.php');
 require('class/question.class.php');
 require('class/quiz.class.php');
 require('class/quiz_score.class.php');
+require('class/tag.class.php');
 require('class/user.class.php');
 
 $path = preg_replace('/wp-content(?!.*wp-content).*/','',__DIR__);
