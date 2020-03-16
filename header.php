0b35eb74fd54fae6042e78689419fab06d3698b6
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-03-16 12:48:09 +0100

    bug isotope solved

--- header.php
+++ header.php
@@ -36,6 +36,8 @@ if(!empty($_SESSION['userConnected']))
         });
 		</script>
 		<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+		<script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.js"></script>
+
 
 	</head>
 	<body>
