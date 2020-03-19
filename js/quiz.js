686036ec6801ea34ffced8ba271e5f56559a779a
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-03-19 11:33:47 +0100

    quiz js in progress for improvement of security towards cheat

--- js/quiz.js
+++ js/quiz.js
@@ -1,68 +1,72 @@
 var url = myScript.theme_directory;
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
-  if(this.readyState == 4 && this.status == 200)
+if(this.readyState == 4 && this.status == 200)
+{
+  var myArray = JSON.parse(this.responseText);
+  console.log(myArray);
+  const grid = document.querySelector(".grid");
+  for($i = 0; $i<myArray.length; $i ++)
   {
-    var myArray = JSON.parse(this.responseText);
-    console.log(myArray);
-    const grid = document.querySelector(".grid");
-    for($i = 0; $i<myArray.length; $i ++)
-    {
-      const gridElement = document.createElement("div");
-      gridElement.classList.add(`element-item` , `${myArray[$i].tag_name}`);
-      gridElement.setAttribute('category', `${myArray[$i].tag_name}`);
-      //---------------------------------------------------------
-      let quizContent = `
-      <span class="tag">${myArray[$i].tag_name}</span>
-      <h3>${myArray[$i].name}</h3>
-      <span class="score">`;
-      console.log( myArray[$i].user_score);
-      if( myArray[$i].user_score != null){
-        quizContent += ``+myArray[$i].user_score+``;
-      }else{
-        quizContent += `0`;
-      }
+    const gridElement = document.createElement("div");
+    gridElement.classList.add(`element-item` , `${myArray[$i].tag_name}`);
+    gridElement.setAttribute('category', `${myArray[$i].tag_name}`);
+    //---------------------------------------------------------
+    let quizContent = `
+    <span class="tag">${myArray[$i].tag_name}</span>
+    <h3>${myArray[$i].name}</h3>
+    <span class="score">`;
+    console.log( myArray[$i].user_score);
+    if( myArray[$i].user_score != null){
+      quizContent += ``+myArray[$i].user_score+``;
+    }else{
+      quizContent += `0`;
+    }
 
-      quizContent +=` pts</span>
-      <div class="imgQ">
-        <img src="${ url + '/img/myAvatar.png'}" alt="photo du quiz"/>
-        <div class="filter"></div>
-      </div>
-    `;
-    if( myArray[$i].user_score == null){
-      quizContent += `<p class="btnQuiz" data-id="${myArray[$i].id}">Jouer</p>`;
-    }
-    //-----------------------------------------------------------------------------
-    gridElement.innerHTML = quizContent;
-      grid.appendChild(gridElement);
-    }
-    let btnQuizs = document.querySelectorAll(".btnQuiz");
-    btnQuizs.forEach(btn => {
-      btn.addEventListener("click", (e)=>{
-        const id = e.target.dataset.id;
-        var urlScript = url + '/play_quiz.php/?id=' + id;
-        console.log(urlScript);
-        var xmlhttp2 = new XMLHttpRequest();
-        xmlhttp2.onreadystatechange = function () {
-          if(this.readyState == 4 && this.status == 200)
+    quizContent +=` pts</span>
+    <div class="imgQ">
+      <img src="${ url + '/img/myAvatar.png'}" alt="photo du quiz"/>
+      <div class="filter"></div>
+    </div>
+    <p class="btnQuiz" data-id="${myArray[$i].id}">Jouer</p>
+  `;
+  // if( myArray[$i].user_score == null){
+  //   quizContent += `<p class="btnQuiz" data-id="${myArray[$i].id}">Jouer</p>`;
+  // }
+  //-----------------------------------------------------------------------------
+  gridElement.innerHTML = quizContent;
+    grid.appendChild(gridElement);
+  }
+  let btnQuizs = document.querySelectorAll(".btnQuiz");
+  btnQuizs.forEach(btn => {
+    btn.addEventListener("click", (e)=>{
+      const id = e.target.dataset.id;
+      var urlScript = url + '/play_quiz.php/?id=' + id;
+      console.log(urlScript);
+      var xmlhttp2 = new XMLHttpRequest();
+      xmlhttp2.onreadystatechange = function () {
+        if(this.readyState == 4 && this.status == 200)
+        {
+          var myQuizz = JSON.parse(this.responseText);
+          if(myQuizz.previous.length > 0)
           {
-            var myQuizz = JSON.parse(this.responseText);
-            const divQuizz = document.createElement("div");
-            divQuizz.classList.add("quizPlay");
-            document.body.appendChild(divQuizz);
-            divQuizz.innerHTML = `
-              <div class="quiz" id="quiz"></div>
-              <div class="btns"> 
-                <button id="next">Prochaine question</button>
-                <button id="submit">Terminer le quiz</button>
-              </div>
-              <div id="results">
-              </div>
-              <div class="timer">
-                <label id="minutes">00</label>:<label id="seconds">00</label>
-              </div>
-            `;
-            
+          console.log(myQuizz.previous);
+          const divQuizz = document.createElement("div");
+          divQuizz.classList.add("quizPlay");
+          document.body.appendChild(divQuizz);
+          divQuizz.innerHTML = `
+          <div class="quiz" id="quiz"></div>
+          <div class="btns"> 
+          <button id="next">Prochaine question</button>
+          <button id="submit">Terminer le quiz</button>
+          </div>
+          <div id="results">
+          </div>
+          <div class="timer">
+          <label id="minutes">00</label>:<label id="seconds">00</label>
+          </div>
+          `;
+          
             let myQuestions = myQuizz.questions;
             const quizContainer = document.getElementById('quiz');
             const resultsContainer = document.getElementById('results');
@@ -94,7 +98,7 @@ xmlhttp.onreadystatechange = function () {
             function shuffle(array)
             {
               array.sort(()=> Math.random()-0.5);
-              console.log(array);
+              // console.log(array);
             }
 
 
@@ -111,18 +115,18 @@ xmlhttp.onreadystatechange = function () {
                 (currentQuestion, questionNumber) => {
                   // variable to store the list of possible answers
                   const currentAnswers = currentQuestion.answers;
+                  
                   const answers = [];
                   numQuestion += 1;
                   // and for each available answer...
                   shuffle(currentAnswers);
-                  console.log(currentAnswers);
+                  // console.log(currentAnswers);
                   for(i = 0; i<currentAnswers.length ; i++){
                     const letters = ['A', 'B' , 'C', 'D'];
                     // ...add an HTML radio button
                     answers.push(
                       `<label>
-                      <input id="${currentAnswers[i].id}" class="input${myQuestions.indexOf(currentQuestion)}" type="radio" name="question${questionNumber}" data-answer="${letters[i]}" value="${currentAnswers[i].is_true}">
-                        
+                        <input id="${currentAnswers[i].id}" class="input${myQuestions.indexOf(currentQuestion)}${[i]}" type="radio" name="question${questionNumber}" data-answer="${letters[i]}" value="${currentAnswers[i].is_true}">
                         <p class="answer">${letters[i]}. ${currentAnswers[i].content}</p>
                       </label>`
                     );
@@ -293,16 +297,20 @@ xmlhttp.onreadystatechange = function () {
             function recupIds(){
 
               id_question = myQuestions[currentSlide].id;
+              id_answer = null;
 
-              var inputs = document.querySelectorAll(`.input${currentSlide}`);
-              
-              inputs.forEach(input => {
+              console.log(myQuestions[currentSlide].answers.length);
+
+              for (let i = 0; i < myQuestions[currentSlide].answers.length; i++) {
+                let input = document.querySelector(`.input${currentSlide}${i}`);
+                console.log(input);
                 if(input.checked)
                 {
                   id_answer = input.id;
+                  return
                 }
-              });
-              
+                console.log(id_answer);
+              }
             }
             
             // Show the first slide
@@ -348,37 +356,42 @@ xmlhttp.onreadystatechange = function () {
             // on submit, show results
             submitButton.addEventListener('click', showResults);
           }
-        };
-
-        // url a trouver
-        xmlhttp2.open("GET", urlScript , true);
-        xmlhttp2.send();
-      })
-    });
-
-    //isotope initialized (with jquery)
-    var $grid = $('.grid').isotope({
-      itemSelector: '.element-item',
-      masonry: {
-        columnWidth: 120,
-        isFitWidth: true
+          // else
+          // {
+          //   alert("coucou");
+          // }
         }
+      };
+
+      // url a trouver
+      xmlhttp2.open("GET", urlScript , true);
+      xmlhttp2.send();
+    })
+  });
+
+  //isotope initialized (with jquery)
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    masonry: {
+      columnWidth: 120,
+      isFitWidth: true
+      }
+  });
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
     });
-    // bind filter button click
-    $('.filters-button-group').on( 'click', 'button', function() {
-      var filterValue = $( this ).attr('data-filter');
-      // use filterFn if matches value
-      $grid.isotope({ filter: filterValue });
-    });
-    // change is-checked class on buttons
-    $('.button-group').each( function( i, buttonGroup ) {
-      var $buttonGroup = $( buttonGroup );
-      $buttonGroup.on( 'click', 'button', function() {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $( this ).addClass('is-checked');
-      });
-    });
-  }
+  });
+}
 };
 
 // url a trouver
