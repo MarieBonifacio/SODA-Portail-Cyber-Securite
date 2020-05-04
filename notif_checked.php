d426141f488937bd46c7fc7eb9dc85fd54740f2d
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-05-04 11:25:36 +0200

    fix notif_checked

--- notif_checked.php
+++ notif_checked.php
@@ -3,6 +3,7 @@ $path = preg_replace('/wp-content(?!.*wp-content).*/','',__DIR__);
 include($path.'wp-load.php');
 
 //maj user meta 
+$user = $_SESSION["user_connected"];
 update_user_meta( $user, $key = 'notification', date("Y-m-d H:i:s") );
 
 
