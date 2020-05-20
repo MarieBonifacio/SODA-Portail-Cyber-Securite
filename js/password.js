6b8cf28cdf1ee6781f8279991a4a038d844f5bbb
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-05-20 11:19:11 +0200

    password js corrected

--- js/password.js
+++ js/password.js
@@ -17,22 +17,18 @@ window.addEventListener("load", ()=>{
     var strength = 0;
     if (password.length>12){
       strength += 1;
-      if (password.match(/[a-z]+/)){
-        strength += 1;
-      }
-      if (password.match(/[A-Z]+/)){
-        strength += 1;
-      }
-      if (password.match(/[0-9]+/)){
-        strength += 1;
-      }
-      if (password.match(/[$@#&!]+/)){
-        strength += 1;
-      }
     }
-    else
-    {
-      strength = 0;
+    if (password.match(/[a-z]+/)){
+      strength += 1;
+    }
+    if (password.match(/[A-Z]+/)){
+      strength += 1;
+    }
+    if (password.match(/[0-9]+/)){
+      strength += 1;
+    }
+    if (password.match(/[$@#&!]+/)){
+      strength += 1;
     }
 
     switch (strength) {
