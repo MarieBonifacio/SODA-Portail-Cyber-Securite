edd0265b9ef36d8cc8bffb6b74a367b8ca871f06
Gauthier Gellé <gelle.gauthier@gmail.com>
2020-06-10 18:02:05 +0200

    css modified for list mod/quiz

--- list_module.php
+++ list_module.php
@@ -1,24 +1,47 @@
-<?php /* Template Name: Liste Module */ 
-$_SESSION['needAdmin'] = true;
-get_header(); 
-cleanSession();
-?>
-
-<h2 class="h2"> Liste Modules </h2>
-
-<div class="modulesL">
-  <div class="listModules">
-    <table>
-      <thead>
-        <tr>
-          <th>Nom</th>
-          <th>Tag</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody class="list"></tbody>
-    </table>
-  </div>
-</div>
-
+<?php /* Template Name: Liste Module */ 
+
+$_SESSION['needAdmin'] = true;
+
+get_header(); 
+
+cleanSession();
+
+?>
+
+
+
+<h2 class="h2"> Liste Modules </h2>
+
+
+
+<div class="modulesL">
+
+  <div class="listModules">
+
+    <table>
+
+      <thead>
+
+        <tr>
+
+          <th>Nom</th>
+
+          <th>Tag</th>
+
+          <th>Mod/Supp</th>
+
+        </tr>
+
+      </thead>
+
+      <tbody class="list"></tbody>
+
+    </table>
+
+  </div>
+
+</div>
+
+
+
 <?php  get_footer(); ?>
--- list_quiz.php
+++ list_quiz.php
@@ -1,24 +1,47 @@
-<?php /* Template Name: Liste Quiz */ 
-$_SESSION['needAdmin'] = true;
-get_header(); 
-cleanSession();
-?>
-
-<h2 class="h2"> Liste des Quizs </h2>
-
-<div class="quizsL">
-  <div class="listQuiz">
-    <table>
-      <thead>
-        <tr>
-          <th>Nom</th>
-          <th>Tag</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody class="list"></tbody>
-    </table>
-  </div>
-</div>
-
+<?php /* Template Name: Liste Quiz */ 
+
+$_SESSION['needAdmin'] = true;
+
+get_header(); 
+
+cleanSession();
+
+?>
+
+
+
+<h2 class="h2"> Liste des Quizs </h2>
+
+
+
+<div class="quizsL">
+
+  <div class="listQuiz">
+
+    <table>
+
+      <thead>
+
+        <tr>
+
+          <th>Nom</th>
+
+          <th>Tag</th>
+
+          <th>Mod/Supp</th>
+
+        </tr>
+
+      </thead>
+
+      <tbody class="list"></tbody>
+
+    </table>
+
+  </div>
+
+</div>
+
+
+
 <?php  get_footer(); ?>
