ce9d408fb4a31c25a3a408bad0ad5dd6ad299e55
MarieBonifacio <mariebonifacio.pro@gmail.com>
2020-06-16 16:03:58 +0200

    campaign back

---
+++ app/update_campaign.php
@@ -0,0 +1,12 @@
+<?php
+define('WP_USE_THEMES', false);
+
+$path = preg_replace('/wp-content(?!.*wp-content).*/','',__DIR__);
+include($path.'wp-load.php');
+
+if(!checkAuthorized(true)){
+    wp_redirect( home_url() );  exit;
+}
+
+
+?>
